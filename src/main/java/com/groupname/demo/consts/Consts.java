c79d1c4b1612801f8009034cae1efe5926031754
zxwzxw904705840 <904705840@qq.com>
2019-12-04 15:30:15 +0800

    新增了注册用户的常量

--- src/main/java/com/groupname/demo/consts/Consts.java
+++ src/main/java/com/groupname/demo/consts/Consts.java
@@ -8,4 +8,14 @@ public class Consts {
 
     public static final String REGISTER_WAITING_FOR_REVIEW = "注册成功，请等待审核！";
     public static final String REGISTER_SUCCESS = "注册成功！";
+    public static final String USERNO_EXISTS = "用户已存在！";
+    public static final String USERNO_IS_TOO_LONG = "用户名过长！";
+    public static final String NAME_IS_EMPTY = "姓名不能为空！";
+    public static final String PASSWORD_NOT_CONSISTENT = "两次密码不一致！";
+    public static final String PASSWORD_TOO_LONG = "密码过长！";
+    public static final String PASSWORD_TOO_SHORT = "密码过短！";
+    public static final String ILLEGAL_TELEPHONE = "不合法的手机号！";
+    public static final String PHONE_IS_EMPTY = "手机号不能为空";
+    public static final String REGISTER_FAIL = "注册失败！";
+
 }
