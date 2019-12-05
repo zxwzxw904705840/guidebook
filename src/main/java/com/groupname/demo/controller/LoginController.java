4841c695a4ad6df675dc9937205394f719a48fe9
zxwzxw904705840 <904705840@qq.com>
2019-12-05 17:46:57 +0800

    更新了登录功能

--- src/main/java/com/groupname/demo/controller/LoginController.java
+++ src/main/java/com/groupname/demo/controller/LoginController.java
@@ -7,23 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import com.alibaba.fastjson.JSON;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-@RestController
+@Controller
 public class LoginController {
     @Autowired
     LoginService loginService;
 
-    @RequestMapping(value = "login",method = RequestMethod.POST)
-    public ResponseEntity<Result> login(@RequestBody Map<String,Object> params){
-        String userNo = params.get("userNo").toString();
-        String password = params.get("password").toString();
+    @RequestMapping(value = "doLogin",method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<Result> doLogin(HttpServletRequest request){
+        System.out.println("login");
+        String userNo = request.getParameter("userNo");
+        String password = request.getParameter("password");
         Result result = loginService.Login(userNo,password);
+        //System.out.println(request.getParameter("password"));
+        /*String userNo = params.get("userNo").toString();
+        String password = params.get("password").toString();
+        Result result = loginService.Login(userNo,password);*/
         return ResponseEntity.ok(result);
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(HttpServletRequest request){
+        return "login";
     }
 
     @RequestMapping(value = "register",method = RequestMethod.POST)
