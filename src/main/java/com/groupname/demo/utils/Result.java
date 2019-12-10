e4615769c0ea06c9cc3738643a02b8355319e94b
zxwzxw904705840 <904705840@qq.com>
2019-12-10 23:27:40 +0800

    增加了时间戳

--- src/main/java/com/groupname/demo/utils/Result.java
+++ src/main/java/com/groupname/demo/utils/Result.java
@@ -1,14 +1,18 @@
 package com.groupname.demo.utils;
 
+import java.util.Date;
+
 public class Result<T> {
     private boolean success;
     private String message;
     private T object;
+    private long timestamp;
 
     public Result(boolean success,String message,T object){
         this.success=success;
         this.message=message;
         this.object=object;
+        this.timestamp=new Date().getTime();
     }
     public Result(boolean success,String message){
         this.success=success;
@@ -24,6 +28,7 @@ public class Result<T> {
     public T getObject(){
         return this.object;
     }
+    public long  getTimestamp(){return this.timestamp;}
     public boolean hasObject(){
         if(object==null){
             return false;
