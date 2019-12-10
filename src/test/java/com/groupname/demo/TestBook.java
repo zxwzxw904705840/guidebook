fabcd2121620d914a7b2787f2f75c96dabeef5da
zxwzxw904705840 <904705840@qq.com>
2019-12-10 17:31:38 +0800

    新增图书

--- src/test/java/com/groupname/demo/TestBook.java
+++ src/test/java/com/groupname/demo/TestBook.java
@@ -1,6 +1,11 @@
 package com.groupname.demo;
 
+import com.groupname.demo.consts.Consts;
+import com.groupname.demo.entity.BookEntity;
+import com.groupname.demo.entity.UserEntity;
 import com.groupname.demo.repository.BookRepository;
+import com.groupname.demo.service.ManagerOperateService;
+import com.groupname.demo.utils.Result;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +16,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TestBook {
     @Autowired
-    BookRepository bookRepository;
+    ManagerOperateService managerOperateService;
     @Test
     public void testBookRepository(){
-        System.out.println(bookRepository.findAll());
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserNo("m10086");
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setIsbn("9787040446081");
+        bookEntity.setPublishingHouse("高等教育出版社");
+        bookEntity.setPublishingData(2019);
+        bookEntity.setNumber(2147483647);
+        bookEntity.setBookName("大学物理");
+        bookEntity.setAuthor("宋更新");
+        bookEntity.setAuthorMajor(Consts.BookMajor.PHYSICS.getValue());
+        Result result = managerOperateService.addBook(bookEntity,userEntity);
+        System.out.println(result.isSuccess()+result.getMessage());
+        //System.out.println(bookRepository.findAll());
     }
     @Test
     public void contextLoads(){
