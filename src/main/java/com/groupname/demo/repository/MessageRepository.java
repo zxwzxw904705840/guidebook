1457f8fbeba229ee7a11b4e52d7448bfcbc60e21
zxwzxw904705840 <904705840@qq.com>
2019-12-10 10:53:02 +0800

    留言表接口

---
+++ src/main/java/com/groupname/demo/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.groupname.demo.repository;
+
+import com.groupname.demo.entity.MessageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<MessageEntity,String> {
+    MessageEntity findByMessageNo(Integer messageNo);
+}
