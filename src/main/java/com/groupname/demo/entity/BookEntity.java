f641db140f10a8bcb6b844ddaaeee3368c2bb884
zxwzxw904705840 <904705840@qq.com>
2019-12-10 16:58:51 +0800

    修改了构造函数

--- src/main/java/com/groupname/demo/entity/BookEntity.java
+++ src/main/java/com/groupname/demo/entity/BookEntity.java
@@ -8,13 +8,17 @@ public class BookEntity {
     private String isbn;
     private String bookName;
     private String author;
-    private String authorMajor;
+    private Integer authorMajor;
     private String titlePage;
     private String publishingHouse;
     private Integer publishingData;
     private String informationUrl;
     private String contentUrl;
     private Integer number;
+
+    public BookEntity(){
+        this.number=0;
+    }
 
     @Id
     @Column(name = "isbn")
@@ -39,16 +43,14 @@ public class BookEntity {
     public String getAuthor() {
         return author;
     }
-    public void setAuthor(String author){
-        this.author=author;
-    }
+    public void setAuthor(String author){ this.author=author; }
 
     @Basic
     @Column(name = "authorMajor")
-    public String getAuthorMajor() {
+    public Integer getAuthorMajor() {
         return authorMajor;
     }
-    public void setAuthorMajor(String authorMajor){
+    public void setAuthorMajor(Integer authorMajor){
         this.authorMajor=authorMajor;
     }
 
