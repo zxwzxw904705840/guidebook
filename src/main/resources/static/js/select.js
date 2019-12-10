30b9eab22821872165493b65c63105d304b5cb14
zxwzxw904705840 <904705840@qq.com>
2019-12-10 10:24:46 +0800

    static

---
+++ src/main/resources/static/js/select.js
@@ -0,0 +1,12 @@
+$('.select').on('click', '.placeholder', function () {
+    var parent = $(this).closest('.select');
+    if (!parent.hasClass('is-open')) {
+        parent.addClass('is-open');
+        $('.select.is-open').not(parent).removeClass('is-open');
+    } else {
+        parent.removeClass('is-open');
+    }
+}).on('click', 'ul>li', function () {
+    var parent = $(this).closest('.select');
+    parent.removeClass('is-open').find('.placeholder').text($(this).text());
+});
